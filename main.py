c719e60a1350a2235bb9bae587b743580a4fb6fc
Nikhil Singhal <nikhilsinghal2019@gmail.com>
2026-08-19 19:25:16 +0530

    add changes

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+print(os.getcwd())
+print(os.listdir(os.getcwd()))
+cur=os.getcwd()
+for i in os.listdir(cur):
+    if os.path.isfile():
+        print(i)
+# print(len(os.listdir(os.getcwd())))
